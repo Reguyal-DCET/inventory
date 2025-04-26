962d07a84fb9b1208fd1edf7a961d2c604b8a422
REGUYAL JENZ D. <reguyaljenzdelosreyes@gmail.com>
2025-04-26 16:08:47 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,96 @@
-// Inventory Management System Functionalities
+// DOM Elements
+const searchInput = document.getElementById('searchInput');
 
-// ========== DOM Elements ========== //
-const tbody = document.querySelector("#inventoryTable tbody");
-const categoryCheckboxes = document.querySelectorAll(".category");
-const sortOption = document.getElementById("sortOption");
-const searchInput = document.getElementById("searchInput");
+// Buttons
+const searchButton = document.getElementById('searchBtn');
+const addItemBtn = document.getElementById('addItemBtn');
 
-const itemModal = document.getElementById("itemModal");
-const itemName = document.getElementById("itemName");
-const itemDetails = document.getElementById("itemDetails");
-const itemQuantity = document.getElementById("itemQuantity");
-const itemPrice = document.getElementById("itemPrice");
-const itemType = document.getElementById("itemType");
-const modalTitle = document.getElementById("modalTitle");
+// Dropdowns
+const searchBy = document.getElementById('searchBy');
+const sortBy = document.getElementById('sortBy');
 
-const openModalBtn = document.querySelector(".button-container button");
-const modalAddBtn = document.querySelector("#itemModal button:nth-of-type(1)");
-const modalCancelBtn = document.querySelector("#itemModal button:nth-of-type(2)");
+// Modals
+const addModal = document.getElementById('addModal');
+const closeAddModal = document.getElementById('closeAddModal');
+const closeEditModal = document.getElementById('closeEditModal');
+const editModal = document.getElementById('editModal');
+
+// Forms
+const addItemForm = document.getElementById('addItemForm');
+const editItemForm = document.getElementById('editItemForm');
+
+const inventoryBody = document.getElementById('inventoryBody');
+
+// Add Inputs
+const itemImage = document.getElementById('itemImage');
+const itemName = document.getElementById('itemName');
+const itemDetails = document.getElementById('itemDetails');
+const itemPrice = document.getElementById('itemPrice');
+const itemQuantity = document.getElementById('itemQuantity');
+
+// Edit Inputs
+const editItemImage = document.getElementById('editItemImage');
+const editItemName = document.getElementById('editItemName');
+const editItemDetails = document.getElementById('editItemDetails');
+const editItemPrice = document.getElementById('editItemPrice');
+const editItemQuantity = document.getElementById('editItemQuantity');
+
+// Event listeners
+// input
+// click
+// onmousedown
+// select
+// change
+// submit
+
+// Inputs
+//searchInput.addEventListener('input', handleSearchInput);
+
+// Buttons
+addItemBtn.addEventListener('click', handleAddItemBtnClick);
+// searchButton.addEventListener('click', handleSearchButtonClick);
+
+// Dropdowns
+// searchBy.addEventListener('change', handleSearchByChange);
+// sortBy.addEventListener('change', handleSortByChange);
+
+// Forms
+addItemForm.addEventListener('submit', handleAddItemFormSubmit);
+// editItemForm.addEventListener('submit', handleEditItemFormSubmit);
+
+// Images
+// itemImage.addEventListener('change', handleItemImageChange);
+// editItemImage.addEventListener('change', handleEditItemImageChange);
+
+// Modals
+closeAddModal.addEventListener('click', handleCloseAddModalClick);
+// closeEditModal.addEventListener('click', handleCloseEditModalClick);
+
+// Functions
+function handleAddItemBtnClick() {
+    addModal.style.display = "block";
+    addModal.style.display = "flex";
+}
+
+function handleCloseAddModalClick() {
+    addModal.style.display = "none";
+}
+
+function handleAddItemFormSubmit(e) {
+    console.log(e);
+    e.preventDefault();
+    const newItem = {
+        id: Date.now(),
+        name: itemName,
+        details: itemDetails,
+        quantity: itemQuantity,
+        price: itemPrice,
+        dateAdded: new Date().toISOString(),
+    };
+
+    console.log(newItem);
+}
+
+// display table with existing products
+// add product
+// edit product
